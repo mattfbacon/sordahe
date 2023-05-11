a7ee5156a199bf53e4cff30f3488e44adbe7bc50
Matt Fellenz <matt@felle.nz>
2023-05-10 20:54:34 -0700

    Clean up dictionary initialization

--- src/dict.rs
+++ src/dict.rs
@@ -1,6 +1,12 @@
+use std::borrow::Borrow;
 use std::collections::HashMap;
+use std::str::FromStr;
 
 use logos::Logos as _;
+use serde::de::{MapAccess, Visitor};
+use serde::{Deserialize, Deserializer};
+use serde_with::DeserializeFromStr;
+use thiserror::Error;
 
 use crate::keys::Keys;
 
@@ -75,17 +81,13 @@ pub enum EntryPart {
 	PloverCommand(PloverCommand),
 }
 
-pub type Entry = Vec<EntryPart>;
-
-#[derive(Debug)]
-pub struct Dict(HashMap<Vec<Keys>, Entry>);
-
-type Raw = HashMap<Box<str>, Box<str>>;
-
-#[derive(Debug)]
-enum ParseError {
+#[derive(Debug, Error)]
+pub enum ParseError {
+	#[error("trailing dash")]
 	TrailingDash,
+	#[error("duplicate key(s) {0:?}")]
 	Duplicate(Keys),
+	#[error("unrecognized character {0:?}")]
 	Unrecognized(char),
 }
 
@@ -205,107 +207,186 @@ fn test_parse_part() {
 	);
 }
 
-fn parse_keys_str(keys: &str) -> Result<Vec<Keys>, ParseError> {
-	let parts = keys.split('/');
-	parts.map(parse_part).collect()
+impl FromStr for Strokes {
+	type Err = ParseError;
+
+	fn from_str(raw: &str) -> Result<Self, ParseError> {
+		let parts = raw.split('/');
+		parts
+			.map(parse_part)
+			.collect::<Result<Vec<_>, _>>()
+			.map(Self)
+	}
 }
 
-#[derive(logos::Logos)]
-enum EntryToken {
-	#[regex(r"([^{]|\\\{)+")]
-	Verbatim,
-	#[regex(r"\{([^}\\]|\\.)*\}")]
-	Special,
-}
+#[derive(Debug, Error)]
+pub enum EntryParseError {}
 
-fn parse_entry(entry: &str) -> Entry {
-	let mut ret = Vec::with_capacity(1);
+fn unescape(escaped: &str) -> Box<str> {
+	let mut ret = String::with_capacity(escaped.len() / 2);
 
-	for (token, span) in EntryToken::lexer(entry).spanned() {
-		let part = match token.expect(entry) {
-			EntryToken::Verbatim => EntryPart::Verbatim(entry[span].trim().into()),
-			EntryToken::Special => {
-				let inner = &entry[span.start + 1..span.end - 1];
-				match inner {
-					"-|" => EntryPart::SetCaps(true),
-					">" => EntryPart::SetCaps(false),
-					"^" => EntryPart::SetSpace(false),
-					" " => EntryPart::SetSpace(true),
-					_ => {
-						if let Some(command) = inner.strip_prefix("PLOVER:") {
-							EntryPart::PloverCommand(command.parse().unwrap())
-						} else if let Ok(punct) = inner.parse() {
-							EntryPart::SpecialPunct(punct)
-						} else {
-							let (strip_before, inner) = inner
-								.strip_prefix('^')
-								.map_or((false, inner), |inner| (true, inner));
-							let (glue, inner) = inner
-								.strip_prefix('&')
-								.map_or((false, inner), |inner| (true, inner));
-							let (strip_after, inner) = inner
-								.strip_suffix('^')
-								.filter(|inner| !inner.ends_with('\\'))
-								.map_or((false, inner), |inner| (true, inner));
-
-							if !(strip_before || strip_after || glue) {
-								eprintln!("warn: pointless curlies around {inner:?}; treating as verbatim");
-							}
-
-							if strip_before {
-								ret.push(EntryPart::SetSpace(false));
-							}
-							if glue {
-								ret.push(EntryPart::Glue);
-							}
-
-							ret.push(EntryPart::Verbatim(inner.into()));
-
-							if strip_after {
-								ret.push(EntryPart::SetSpace(false));
-							}
-
-							continue;
-						}
-					}
-				}
+	let mut chars = escaped.chars();
+	while let Some(ch) = chars.next() {
+		ret.push(if ch == '\\' {
+			let escape = chars.next().unwrap();
+			match escape {
+				'^' | '{' | '}' | '\\' => escape,
+				_ => unreachable!("escape {escape:?}"),
 			}
-		};
-		ret.push(part);
+		} else {
+			ch
+		});
 	}
 
-	ret
+	ret.into()
+}
+
+impl FromStr for Entry {
+	type Err = EntryParseError;
+
+	fn from_str(entry: &str) -> Result<Self, Self::Err> {
+		#[derive(logos::Logos)]
+		enum EntryToken {
+			#[regex(r"([^{]|\\\{)+")]
+			Verbatim,
+			#[regex(r"\{([^}\\]|\\.)*\}")]
+			Special,
+		}
+
+		let mut ret = Vec::with_capacity(1);
+
+		for (token, span) in EntryToken::lexer(entry).spanned() {
+			match token.expect(entry) {
+				EntryToken::Verbatim => {
+					let text = entry[span].trim();
+					if !text.is_empty() {
+						ret.push(EntryPart::Verbatim(unescape(text)));
+					}
+				}
+				EntryToken::Special => {
+					let inner = &entry[span.start + 1..span.end - 1];
+					let part = match inner {
+						"-|" => EntryPart::SetCaps(true),
+						">" => EntryPart::SetCaps(false),
+						"^" => EntryPart::SetSpace(false),
+						" " => EntryPart::SetSpace(true),
+						_ => {
+							if let Some(command) = inner.strip_prefix("PLOVER:") {
+								EntryPart::PloverCommand(command.parse().unwrap())
+							} else if let Ok(punct) = inner.parse() {
+								EntryPart::SpecialPunct(punct)
+							} else {
+								let (strip_before, inner) = inner
+									.strip_prefix('^')
+									.map_or((false, inner), |inner| (true, inner));
+								let (glue, inner) = inner
+									.strip_prefix('&')
+									.map_or((false, inner), |inner| (true, inner));
+								let (strip_after, inner) = inner
+									.strip_suffix('^')
+									.filter(|inner| !inner.ends_with('\\'))
+									.map_or((false, inner), |inner| (true, inner));
+
+								if !(strip_before || strip_after || glue) {
+									eprintln!("warn: pointless curlies around {inner:?}; treating as verbatim");
+								}
+
+								if strip_before {
+									ret.push(EntryPart::SetSpace(false));
+								}
+								if glue {
+									ret.push(EntryPart::Glue);
+								}
+
+								ret.push(EntryPart::Verbatim(unescape(inner)));
+
+								if strip_after {
+									ret.push(EntryPart::SetSpace(false));
+								}
+
+								continue;
+							}
+						}
+					};
+					ret.push(part);
+				}
+			}
+		}
+
+		Ok(Self(ret))
+	}
 }
 
 #[test]
 fn test_parse_entry() {
 	assert_eq!(
-		parse_entry("{>}{&p}"),
+		r"{>} {&p\^}".parse::<Entry>().unwrap().0,
 		vec![
 			EntryPart::SetCaps(false),
 			EntryPart::Glue,
-			EntryPart::Verbatim("p".into()),
+			EntryPart::Verbatim("p^".into()),
 		]
 	);
 }
 
+#[derive(Debug, PartialEq, Eq, DeserializeFromStr)]
+pub struct Entry(pub Vec<EntryPart>);
+
+#[derive(Debug, PartialEq, Eq, Hash, DeserializeFromStr)]
+pub struct Strokes(pub Vec<Keys>);
+
+impl Borrow<[Keys]> for Strokes {
+	fn borrow(&self) -> &[Keys] {
+		&self.0
+	}
+}
+
+#[derive(Debug)]
+pub struct Dict(HashMap<Strokes, Entry>);
+
+impl<'de> Deserialize<'de> for Dict {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: Deserializer<'de>,
+	{
+		struct MapVisitor {}
+
+		impl<'de> Visitor<'de> for MapVisitor {
+			type Value = Dict;
+
+			fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+				formatter.write_str("a string-to-string map")
+			}
+
+			#[inline]
+			fn visit_map<A>(self, mut access: A) -> Result<Self::Value, A::Error>
+			where
+				A: MapAccess<'de>,
+			{
+				let mut values = HashMap::with_capacity(access.size_hint().unwrap_or(0));
+
+				while let Some((key, value)) = access.next_entry()? {
+					if let Some(old) = values.get(&key) {
+						eprintln!("warn: overlap on {key:?}; prev was {old:?}, current is {value:?}");
+					}
+					values.insert(key, value);
+				}
+
+				Ok(Dict(values))
+			}
+		}
+
+		let visitor = MapVisitor {};
+		deserializer.deserialize_map(visitor)
+	}
+}
+
 impl Dict {
 	pub fn load() -> Self {
-		let raw: Raw = serde_json::from_str(&std::fs::read_to_string("dict.json").unwrap()).unwrap();
-		let mut map = HashMap::with_capacity(raw.len());
-		for (k, v) in raw
-			.into_iter()
-			.map(|(keys, output)| (parse_keys_str(&keys).expect(&keys), parse_entry(&output)))
-		{
-			if let Some(old) = map.get(&k) {
-				eprintln!("warn: overlap on {k:?}; prev was {old:?}, current is {v:?}");
-			}
-			map.insert(k, v);
-		}
-		Self(map)
+		serde_json::from_str(&std::fs::read_to_string("dict.json").unwrap()).unwrap()
 	}
 
-	pub fn get(&self, keys: &[Keys]) -> Option<&[EntryPart]> {
-		self.0.get(keys).map(|s| &**s)
+	pub fn get(&self, keys: &[Keys]) -> Option<&Entry> {
+		self.0.get(keys)
 	}
 }
